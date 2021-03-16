74fa926d952c450cd895edc03820c5c48be63b77
Ynda Jas <yndajas@gmail.com>
2021-03-16 01:47:04 +0000

    commit tree 05cf3e68052931b93d941e1a79b2c700d7ada1cd

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Ynda is the champion"
+document.body.appendChild(newHeader);
